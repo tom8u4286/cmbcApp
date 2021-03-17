80607cd62a90f6e3b460f7af94b021387cf74a68
tom8u4286 <tom8u4286@gmail.com>
2021-03-17 09:24:22 +0800

    Initial Commit

---
+++ NavigationViewPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationViewPractice
+//
+//  Created by 曲奕帆 on 2021/3/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationViewPractice/NavigationViewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewPracticeApp.swift
+//  NavigationViewPractice
+//
+//  Created by 曲奕帆 on 2021/3/17.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
